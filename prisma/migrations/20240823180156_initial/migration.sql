cab644353aa02b27c4f6c8d57e4a637ec6989253
Serpentariu13 <serpentarium13@mail.ru>
2024-08-23 21:02:06 +0300

    db: migration

---
+++ prisma/migrations/20240823180156_initial/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `Thought` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` TEXT NOT NULL,
+    `text` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
